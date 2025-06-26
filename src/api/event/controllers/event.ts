2748fd5c42552c42b4bb2f26a90efe343bf8621f
NizZya <test@email.com>
2025-06-26 21:37:09 +0200

    feat: add allow-event-owner policy and custom event controller

--- src/api/event/controllers/event.ts
+++ src/api/event/controllers/event.ts
@@ -1,7 +1,8 @@
-/**
- * event controller
- */
+import { factories } from '@strapi/strapi';
 
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::event.event');
+export default factories.createCoreController('api::event.event', ({ strapi }) => ({
+  async create(ctx) {
+    ctx.request.body.data.user = ctx.state.user.id;
+    return await super.create(ctx);
+  }
+}));
--- src/api/event/routes/event.ts
+++ src/api/event/routes/event.ts
@@ -1,7 +1,19 @@
-/**
- * event router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::event.event');
+export default [
+  {
+    method: 'PUT',
+    path: '/events/:id',
+    handler: 'event.update',
+    config: {
+      policies: ['api::event.allow-event-owner'],
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/events/:id',
+    handler: 'event.delete',
+    config: {
+      policies: ['api::event.allow-event-owner'],
+    },
+  },
+  // ...інші роутери
+];
